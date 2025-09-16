2ce4ca58e137cee53c73d7ad6dbf98cd0136f938
leocho12 <hjc021212@gmail.com>
2025-09-16 11:29:17 +0900

    complete top move

--- character_moves.py
+++ character_moves.py
@@ -7,19 +7,27 @@ grass=load_image('grass.png')
 character=load_image('character.png')
 
 
+
 def move_top():
+    print("move top")
+    for x in range(0,800,5):
+        draw_character(x,550)
+
     pass
 
 
 def move_right():
+    print("move right")
     pass
 
 
 def move_bottom():
+    print("move bottom")
     pass
 
 
 def move_left():
+    print("move left")
     pass
 
 
@@ -40,17 +48,21 @@ def move_circle():
         r=200
         x=r*math.cos(math.radians(deg))+400
         y=r*math.sin(math.radians(deg))+300
-        clear_canvas_now()
-        character.draw_now(x,y)
-        delay(0.01)
+        draw_character(x, y)
     pass
+
+
+def draw_character(x: float, y: float):
+    clear_canvas_now()
+    character.draw_now(x, y)
+    delay(0.01)
 
 
 while(True):
 
 
     # 원형 이동
-    move_circle()
+    #move_circle()
     #사각 이동
     move_rect()
 
