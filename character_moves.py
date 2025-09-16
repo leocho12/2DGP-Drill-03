408ae8b89cb8832d3535a2ba42c4939cb2e9b0f6
leocho12 <hjc021212@gmail.com>
2025-09-16 10:50:52 +0900

    사각,원형 이동함수 선언

--- character_moves.py
+++ character_moves.py
@@ -6,8 +6,21 @@ open_canvas()
 grass=load_image('grass.png')
 character=load_image('character.png')
 
+
+def move_rect():
+    pass
+
+
+def move_circle():
+    pass
+
+
 while(True):
 
     pass
+    #사각 이동
+    move_rect()
+    #원형 이동
+    move_circle()
 
 close_canvas()
